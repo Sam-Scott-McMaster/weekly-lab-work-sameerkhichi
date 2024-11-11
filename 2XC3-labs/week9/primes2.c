dcaef3103fba8b811e953c366b0d58e150074fff
sameerkhichi <khichis@mcmaster.ca>
2024-11-11 14:16:15 -0500

    adding all the exercises from lab 9.1

---
+++ 2XC3-labs/week9/primes2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define QUICKCHECK(n) (n < 2)
+#define CHECKFACTOR(a, b) (a % b == 0)
+
+
+/*
+bool quickcheck(int n) {
+    return n < 2;
+}
+
+bool checkfactor(int a, int b) {
+    return a % b == 0;
+}
+*/
+
+bool isprime(int n) {
+    if (QUICKCHECK(n)) 
+        return false;
+
+    for (int factor = 2; factor < n; factor++) 
+        if (CHECKFACTOR(n, factor))
+            return false;
+
+    return true;
+}
+
+void main() {
+    long start = clock();
+    for (int n=-200000; n<=200000; n++)
+        if (isprime(n)) 
+            printf("%d ",n);
+    long end = clock();
+    printf("\nRunning time: %.4f seconds\n",(float)(end-start)/1000000);
+}
